0c76865a453512394965ab897100b58c2b0afb6f
tanya malik <tanyamalik@tanyas-MacBook-Air.local>
2026-02-01 23:48:46 +0530

    fix app for deployment

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import mysql.connector
-
-app = Flask(__name__)
+import os
+app = Flask(__name__, static_folder="static", template_folder="templates")
 app.secret_key = "lostandfound123"
 
-db = mysql.connector.connect(
+#db = mysql.connector.connect(
     host="localhost",
     user="root",
     password="Radhika1@",
     database="lostfound"
 )
-cursor = db.cursor(buffered=True)
-def reconnect_db():
-    global db, cursor
-    if not db.is_connected():
-        db.reconnect(attempts=3, delay=2)
-        cursor = db.cursor(buffered=True)
+#cursor = db.cursor(buffered=True)
+#def reconnect_db():
+ #   global db, cursor
+  #  if not db.is_connected():
+   #     db.reconnect(attempts=3, delay=2)
+    #    cursor = db.cursor(buffered=True)
 
 
 # ---------------- LOGIN PAGE ----------------
@@ -211,4 +211,4 @@ def delete_found(id):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0," port=int(os.environ.get("PORT",5000)))
